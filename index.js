9b7ce9dd54be1996b2fb8d4df925224e1384d3ff
theedu007 <eabigboy4@gmail.com>
2019-11-16 19:20:33 -0600

    Creating file structure and testing

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const server = express();
+const path = require('path');
+
+server.use(express.static(path.join(__dirname + '/public')));
 
 server.get('/',(request, response) => {
-    response.send(":D");
-})
+    response.sendFile(path.join(__dirname + '/html/index.html'));
+});
 
 server.listen("5000");
---
+++ public/index.js
@@ -0,0 +1,3 @@
+$(document).ready(() => {
+    $("#main-content").append("hola :D");
+});
